1936f3718d15c0169510ceea05632ff8175c5226
Harley <127766078+moonharl@users.noreply.github.com>
2024-06-07 16:07:46 -0300

    Update flash.js

--- main/js/flash.js
+++ main/js/flash.js
@@ -1,12 +1,10 @@
-function trackMouse(event) {
-  document.documentElement.style.setProperty(
-    '--cursorXPos',
-    `${event.clientX}px`
-  )
-  document.documentElement.style.setProperty(
-    '--cursorYPos',
-    `${event.clientY}px`
-  )
+function update(e){
+  var x = e.clientX || e.touches[0].clientX
+  var y = e.clientY || e.touches[0].clientY
+
+  document.documentElement.style.setProperty('--cursorX', x + 'px')
+  document.documentElement.style.setProperty('--cursorY', y + 'px')
 }
-​
-document.addEventListener('mousemove', trackMouse)
+
+document.addEventListener('mousemove',update)
+document.addEventListener('touchmove',update)
