55152185c40ee8bd802298622d10466a257635a3
Harley <127766078+moonharl@users.noreply.github.com>
2024-06-07 15:55:37 -0300

    Create flash.js

---
+++ main/js/flash.js
@@ -0,0 +1,12 @@
+function trackMouse(event) {
+  document.documentElement.style.setProperty(
+    '--cursorXPos',
+    `${event.clientX}px`
+  )
+  document.documentElement.style.setProperty(
+    '--cursorYPos',
+    `${event.clientY}px`
+  )
+}
+​
+document.addEventListener('mousemove', trackMouse)
