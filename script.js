881a521124e7777ae674bddbe6223f2c8d033d53
Harley <127766078+moonharl@users.noreply.github.com>
2025-02-09 00:03:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ document.addEventListener("keydown", function(event) {
     if (event.ctrlKey && (event.key === "u" || event.key === "U")) {
         event.preventDefault();
     }
-    if (event.ctrlKey && event.shiftKey && (event.key === "i" || event.key === "I") {
+    if (event.ctrlKey && event.shiftKey && event.key === "I") {
         event.preventDefault();
+    }
+    if (event.key === "F12") {
+        event.preventDefault();
+    }
+    if (event.ctrlKey && event.shiftKey && event.key === "C") {
+        event.preventDefault();
+    }
+    if (event.ctrlKey && event.shiftKey && event.key === "J") {
+        event.preventDefault();
+    }
+    if (event.key === "F11") {
+        event.preventDefault();
+    }
+    if (event.ctrlKey && event.shiftKey && event.key === "F") {
+        event.preventDefault();
+    }
 });
 
 document.addEventListener("selectstart", function(event) {
