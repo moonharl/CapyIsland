2bafc42f563eb1c420d587119da55a99c1d3b74f
Harley <127766078+moonharl@users.noreply.github.com>
2025-02-08 23:53:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.addEventListener("keydown", function(event) {
     if (event.ctrlKey && (event.key === "u" || event.key === "U")) {
         event.preventDefault();
     }
+    if (event.ctrlKey && event.shiftKey && (event.key === "i" || event.key === "I") {
+        event.preventDefault();
 });
 
 document.addEventListener("selectstart", function(event) {
