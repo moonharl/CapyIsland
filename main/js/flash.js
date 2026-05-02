874ae3067af91126ab94a981165d8a246cc6376c
Harley <127766078+moonharl@users.noreply.github.com>
2026-05-02 20:23:04 -0300

    Update flash.js

--- main/js/flash.js
+++ main/js/flash.js
@@ -1,7 +1,7 @@
-const shadow = document.querySelector('.flashlight');
+const flashlight = document.querySelector('.flashlight');
 
 document.addEventListener('mousemove', (e) => {
   let x = e.clientX - (document.documentElement.clientWidth * 1.5);
   let y = e.clientY - (document.documentElement.clientHeight * 1.5);
-  shadow.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  flashlight.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 })
